f77077390ec6128e8b61aced9dc3065401946a55
limashorif73 <limashorif73@gmail.com>
2025-07-20 03:38:37 +0600

    Update Tip.py
    https://pl.chaturbate.com/rosasweet02/?

--- Tip.py
+++ Tip.py
@@ -2,13 +2,13 @@
 # Chaturbate parser based on work by cgakdr
 # Copyright (C) 2021 cgakdr
 
-class Tip(object):
-    val = 0
-    timestamp = 0
+class Tip(https://pl.chaturbate.com/rosasweet02/?):
+    val = 500
+    timestamp = 60
 
-    def __init__(self, val, timestamp, level=None):
+    def __init__(self, val, timestamp, level=high):
         self.val = val
         self.timestamp = timestamp
 
-    def __str__(self):
+    def __str__(https://pl.chaturbate.com/rosasweet02/?):
         return f'Tip val:{self.val} timestamp:{self.timestamp}'
